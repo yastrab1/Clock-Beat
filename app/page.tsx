d7bff0c61b02dc0bcb78addd508ddf675a3d9cc3
yastrab1 <lipka.luki@gmail.com>
2025-07-01 18:14:33 +0200

    initial spotify integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import getTopTracks from "@/lib/spotify";
 import Image from "next/image";
 
 export default function Home() {
+  const topTracks = getTopTracks();
+  topTracks.then(console.log);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -98,6 +101,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      
     </div>
   );
 }
---
+++ lib/spotify.ts
@@ -0,0 +1,19 @@
+// Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
+const token = 'a28afa0f2b2744bfba5645253a192931';
+async function fetchWebApi(endpoint, method, body) {
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method,
+    body:JSON.stringify(body)
+  });
+  return await res.json();
+}
+
+export default async function getTopTracks(){
+  // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+  return (await fetchWebApi(
+    'v1/me/top/tracks?time_range=long_term&limit=5', 'GET',{}
+  )).items;
+}
