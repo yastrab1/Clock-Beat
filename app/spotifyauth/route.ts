50df4e2925505829da38bdf193dfaf41342a9535
yastrab1 <lipka.luki@gmail.com>
2025-07-01 18:38:03 +0200

    implemented oauth exchange

--- app/spotifyauth/route.ts
+++ app/spotifyauth/route.ts
@@ -2,16 +2,31 @@ export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const code = searchParams.get('code');
     
+    const exchangeResponse = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Basic ${Buffer.from(process.env.SPOTIFY_CLIENT_ID+":"+process.env.SPOTIFY_CLIENT_SECRET).toString('base64')}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: {
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: process.env.SPOTIFY_REDIRECT_URI || ''
+        } as any
+    });
+    const tokenResponse = await exchangeResponse.json();
+    console.log(tokenResponse);
+    const accessToken = tokenResponse.access_token;
 
     const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing?additional_types=track', {
         method: 'GET',
         headers: {
-            'Authorization': `Bearer ${code}`
+            'Authorization': `Bearer ${accessToken}`
         }
     });
 
-    const data = await response.json();
-    return new Response(JSON.stringify(data), {
+    const currentTrack = await response.json();
+    return new Response(JSON.stringify(currentTrack), {
         headers: { 'Content-Type': 'application/json' }
     });
 }
